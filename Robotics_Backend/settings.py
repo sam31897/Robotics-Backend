1342ee52db03406d7186363da489d5572b0b4e8c
Sam <shuzawa@usc.edu>
2018-09-16 15:56:46 -0700

    url and settings.py edited

--- Robotics_Backend/settings.py
+++ Robotics_Backend/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# File upload
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- uploadVideo/views.py
+++ uploadVideo/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
@@ -9,4 +10,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def uploadVideo(request):
+    if request.method != 'POST':
+        return JsonResponse({'status': 400, 'message': "Error, please use POST." }, status=400)
+
+    
     return
