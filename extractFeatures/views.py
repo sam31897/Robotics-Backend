3d4d53e86e5911b17c545509a6565e38b7238edf
Sam <shuzawa@usc.edu>
2018-09-16 14:52:49 -0700

    import library

--- extractFeatures/views.py
+++ extractFeatures/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from subprocess import call #library for command line calls
 
 # Create your views here.
 
