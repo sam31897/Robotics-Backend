5ce084424944d4684b415b5f5b9ab18e1dc68d4f
Sam <shuzawa@usc.edu>
2018-09-18 14:05:32 -0700

    enabled CORS

--- Robotics_Backend/settings.py
+++ Robotics_Backend/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +126,6 @@ STATIC_URL = '/static/'
 # File upload
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# CORS whitelist
+CORS_ORIGIN_ALLOW_ALL = True
